3024c012c5d614de0db835bd2f12ad4a1466ac6e
ragaes-ui <ragaesa75@gmail.com>
2026-07-31 15:57:41 +0700

    fix balikin

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
-import "./css/style.css";
+import "./style.css";
 import m from "mithril";
 import ConcertLayout from "./views/ConcertLayout.js";
-import AdminLayout from "./views/AdminLayout.js";
 
 const appContainer = document.getElementById("app");
 
-// Perbaikan Rute: Daftarkan /home secara spesifik supaya tidak direbut oleh /:id
+// Menggunakan m.route untuk sistem pindah laman yang sesungguhnya
 m.route(appContainer, "/home", {
-  "/home": ConcertLayout,
-  "/admin/dashboard": AdminLayout,
   "/:id": ConcertLayout
 });
