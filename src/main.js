1c1152b85bde8873f849dfe57557b097b644eac3
ragaes-ui <ragaesa75@gmail.com>
2026-08-01 10:02:37 +0700

    Update routes to clean single layout

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import "./css/style.css";
 import m from "mithril";
 import ConcertLayout from "./views/ConcertLayout.js";
+import AdminLayout from "./views/AdminLayout.js";
 
 const appContainer = document.getElementById("app");
 
-// Menggunakan m.route untuk sistem pindah laman yang sesungguhnya
+// Perbaikan Rute: Daftarkan /home secara spesifik supaya tidak direbut oleh /:id
 m.route(appContainer, "/home", {
+  "/home": ConcertLayout,
+  "/admin/dashboard": AdminLayout,
   "/:id": ConcertLayout
 });
