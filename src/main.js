53bc904df2e3b1af6c955d38967ea068828448e7
ragaes-ui <ragaesa75@gmail.com>
2026-08-01 11:44:52 +0700

    penambahan loading

--- src/main.js
+++ src/main.js
@@ -10,4 +10,18 @@ m.route(appContainer, "/home", {
   "/home": ConcertLayout,
   "/admin/dashboard": AdminLayout,
   "/:id": ConcertLayout
+}); // <--- m.route ditutup di sini
+
+// --- LOGIKA UNTUK MENGHILANGKAN SPLASH SCREEN LOADING ---
+window.addEventListener("load", () => {
+  const loader = document.getElementById("global-loader");
+  if (loader) {
+    // Ubah opacity jadi 0 agar perlahan memudar (fade out)
+    loader.style.opacity = "0";
+    
+    // Hapus elemen dari HTML setelah animasinya selesai (700ms)
+    setTimeout(() => {
+      loader.remove();
+    }, 700);
+  }
 });
